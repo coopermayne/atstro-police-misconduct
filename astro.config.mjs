493977c1127a0dfd8ab3bfdd024dc2728a428465
Cooper Mayne <coopermayne@gmail.com>
2025-04-29 21:25:52 -0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()]
-	}
+  vite: {
+      plugins: [tailwindcss()]
+	},
+
+  integrations: [
+    sanity({
+      projectId: "i5q3udyt",
+      dataset: "production",
+      useCdn: false, // for static builds
+    }),
+  ],
 });
