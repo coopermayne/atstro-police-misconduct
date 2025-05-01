fea02b9514c80790211c9b64ff19a5404bbc1995
Cooper Mayne <coopermayne@gmail.com>
2025-05-01 21:44:07 +0000

    working navbar component

---
+++ src/scripts/menuToggle.js
@@ -0,0 +1,14 @@
+console.log('Setting up menu toggle');
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuOpenIcon = document.getElementById('menu-open-icon');
+const menuCloseIcon = document.getElementById('menu-close-icon');
+
+if (menuToggle) {
+  menuToggle.addEventListener('click', () => {
+    const isHidden = mobileMenu.classList.contains('hidden');
+    mobileMenu.classList.toggle('hidden', !isHidden);
+    menuOpenIcon.classList.toggle('hidden', !isHidden);
+    menuCloseIcon.classList.toggle('hidden', isHidden);
+  });
+}
