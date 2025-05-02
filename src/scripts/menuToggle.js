3a242eb127d19b3fa93a7c648952a7194a17a9bc
Cooper Mayne <coopermayne@gmail.com>
2025-05-02 07:12:40 +0000

    Enhance mobile menu functionality: Improve menu toggle behavior and add overlay click handling for better user experience.

--- src/scripts/menuToggle.js
+++ src/scripts/menuToggle.js
@@ -3,12 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuOpenIcon = document.getElementById("menu-open-icon");
     const menuCloseIcon = document.getElementById("menu-close-icon");
     const mobileMenu = document.getElementById("mobile-menu");
+    const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
+
 
     menuToggle.addEventListener("click", () => {
         // Toggle the mobile menu visibility
         mobileMenu.classList.toggle("hidden");
+        mobileMenuOverlay.classList.toggle('hidden');
 
         // Toggle the visibility of the icons
+        menuOpenIcon.classList.toggle("hidden");
+        menuCloseIcon.classList.toggle("hidden");
+    });
+
+    mobileMenuOverlay.addEventListener('click', () => {
+        // Hide the mobile menu and overlay when the overlay is clicked
+        mobileMenu.classList.toggle("hidden");
+        mobileMenuOverlay.classList.toggle('hidden');
+
+        // Show the open icon and hide the close icon
         menuOpenIcon.classList.toggle("hidden");
         menuCloseIcon.classList.toggle("hidden");
     });
